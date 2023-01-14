0ec789f3b871124c7b40eb52f1ddbe50f7f75e60
Konstantin-Ustinov <ustinov.kn@mail.ru>
2023-01-14 23:50:48 +0300

    first commit

---
+++ src/main/java/com/scool/highscool/HighScoolApplication.java
@@ -0,0 +1,13 @@
+package com.scool.highscool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HighScoolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HighScoolApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/scool/highscool/HighScoolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scool.highscool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HighScoolApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
